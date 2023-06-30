b4b11abece5eeeebba5efd4078675bf90ccfabe9
deepwubs <pchucklambs@gmail.com>
2023-06-29 23:19:47 -0500

    event listener on saveBtn

--- Assets/js/script.js
+++ Assets/js/script.js
@@ -2,16 +2,18 @@
 // the code isn't run until the browser has finished rendering all the elements
 // in the html.
 $(function () {
-  
+
   var currentHour = dayjs().format('H')
 
-  // TODO: Add a listener for click events on the save button. This code should
-  // use the id in the containing time-block as a key to save the user input in
-  // local storage. HINT: What does `this` reference in the click listener
-  // function? How can DOM traversal be used to get the "hour-x" id of the
-  // time-block containing the button that was clicked? How might the id be
-  // useful when saving the description in local storage?
-  //
+  // Click events for save buttons.
+  $('.saveBtn').on('click', function() {
+    var timeBlockId = $(this).parent().attr('id');
+    var userInput = $(this).siblings('.description').val();
+    
+    // TODO: 
+    // Save the user input in local storage using the time-block ID as the key
+    console.log(timeBlockId, userInput);
+  });
 
   // Code to apply the past, present, or future class to each time
   $('.time-block').each(function() {
@@ -27,15 +29,11 @@ $(function () {
     }
   });
 
-  // How can Day.js be used to get the current hour in 24-hour time?
-  console.log(currentHour)
-
-
-
   // TODO: Add code to get any user input that was saved in localStorage and set
   // the values of the corresponding textarea elements. HINT: How can the id
   // attribute of each time-block be used to do this?
-  //
+
+
 
   // Code to display the current date in the header of the page.
   var today = dayjs();
